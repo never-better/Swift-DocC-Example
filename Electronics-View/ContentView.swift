843ccfb3f4fe82ca9a3eba36c543e5873029eebd
never-better <youtaktak@gmail.com>
2023-07-04 21:37:11 +0900

    first commit

---
+++ Electronics-View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Electronics-View
+//
+//  Created by youtak on 2023/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Electronics-View/Electronics_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Electronics_ViewApp.swift
+//  Electronics-View
+//
+//  Created by youtak on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct Electronics_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
